f0dddb6c895fbef111ea15bd3304b4df1a0e0073
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-05-06 09:44:12 +0530

    add the key to the chec and its succesful response

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Products, Navbar } from "./components";
+import { commerce } from "./lib/commerce";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  //Fetching the products on app start
+  const fetchProducts = async ()=> {
+    //Destructing the data that we get on the response after awaiting for it
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  }
+
+  //Only run at the start.
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
   return (
     <div>
       <Navbar />
