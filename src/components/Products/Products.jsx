dd723785ac5bc43fb9bc008f5aae5b5e61d11166
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-05-08 11:49:44 +0530

    populating the items from commerce.js

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,16 +3,8 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 //Products array for temporary use
-const products = [
-    {id: 1, name: 'Shoes', description: 'Running Shoes', price: '$5', 'image': 'https://images.unsplash.com/photo-1533867617858-e7b97e060509?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80'},
-    {id: 2, name: 'Macbook', description: 'Higher end Laptops', price: '$8.75', 'image': 'https://images.unsplash.com/photo-1613502971768-d0b2138cc2db?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8bWFjYm9vayUyMHByb3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'},
-    {id: 3, name: 'Laptop', description: 'Medium range laptop', price: '$7.54', 'image': 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'},
-    {id: 4, name: 'Television', description: 'best Televisions in the category',price: '$12', 'image': 'https://images.unsplash.com/photo-1611484543726-c3685163acfe?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'}
-];
-products.map((product) => {
-    return console.log(product);
-});
-const Products = () => {
+
+const Products = ({ products }) => {
     const classes = useStyles();
     return(
     <main className={classes.content}>
